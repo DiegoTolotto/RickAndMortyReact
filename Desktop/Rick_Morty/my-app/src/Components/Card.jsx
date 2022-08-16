4cd297db1d37e3cf9d4a0e1290f909ee66322649
DiegoTolotto8@gmail.com <diego.tolotto8@gmail.com>
2022-08-16 16:28:38 -0300

    First commit

---
+++ Desktop/Rick_Morty/my-app/src/Components/Card.jsx
@@ -0,0 +1,18 @@
+import { React, useState, useEffect } from "react";
+import axios from "axios";
+
+export default Cards() {
+
+    const getApi = () => {
+       axios 
+       .get(`https://rickandmortyapi.com/api`)
+        
+        
+    }
+    
+    return (
+        <div>
+              <h1>ola mundo</h1>
+        </div>
+    )
+};
