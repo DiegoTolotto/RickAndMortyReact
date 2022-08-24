1f8046fb2e043791f2c86c8786fede8e72c26e09
DiegoTolotto8@gmail.com <diego.tolotto8@gmail.com>
2022-08-24 13:04:36 -0300

    Corrigindo erro de requisição

--- Desktop/Rick_Morty/my-app/src/Components/Card.jsx
+++ Desktop/Rick_Morty/my-app/src/Components/Card.jsx
@@ -4,12 +4,18 @@ export default function Card({ cards }) {
   return (
     <div className="card">
       {cards.map((itens, index) => (
-        <div key={index} className="col">
-          <img src={itens.image} alt=""/>
-
-          <p>Nome: {itens.name}</p>
+        <div key={index} className="col-lg-3 col-md-6 col-sm-12 mb-4">
+        <div className="card" style={{ minWidth: "200px" }}>
+          <img className="card-img-top" src={itens.image} alt="character" />
+          <div className="card-body">
+            <h5 className="card-title">{itens.name}</h5>
+            <hr />
+            <p className="card-text">Species: {itens.species}</p>
+            <p className="card-text">Location: {itens.location.name}</p>
+          </div>
         </div>
-      ))}
+      </div>
+    ))}
     </div>
   )
 };
